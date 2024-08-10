c571b78990fe08578c74ca948c38b3fbe28d1d02
Kareem <kareem@lukitomo.com>
2024-08-10 19:44:46 +0700

    feat: Import VM-501 and VM-502 configurations (#6)

--- import.tf
+++ import.tf
@@ -7,3 +7,13 @@ import {
   id = "pve/qemu/500"
   to = proxmox_vm_qemu.vm-500
 }
+
+import {
+  id = "pve/qemu/501"
+  to = proxmox_vm_qemu.vm-501
+}
+
+import {
+  id = "pve/qemu/502"
+  to = proxmox_vm_qemu.vm-502
+}
